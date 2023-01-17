eed3792e840c8fd2157def06bffe799de7bab35b
AnindyaBaule <banindya99@gmail.comm>
2023-01-18 01:50:44 +0600

    pollution check feature

--- app.js
+++ app.js
@@ -1,39 +1,94 @@
-var apiKey="c3c66156b36559256410e2635fc229d3";
+var apiKey = "c3c66156b36559256410e2635fc229d3";
 
-var input=document.getElementById("InpuTweather")
-var result=document.getElementById("showResult")
+var input = document.getElementById("InpuTweather")
+var result = document.getElementById("showResult")
 
-function btn(){
-    var cityTem=input.value;
+function btn() {
+    var cityTem = input.value;
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityTem}&appid=${apiKey}`)
-    .then(res => res.json())
-    .then (data =>check(data))
-   
+        .then(res => res.json())
+        .then(data => check(data))
+
+
 }
-function check(data){
-    if(data.message){
-        result.innerHTML=`<h1>No City Found</h1>`
+
+
+function check(data) {
+    if (data.message) {
+        result.innerHTML = `<h1>No City Found</h1>`
     }
-    else{
+    else {
         dataRec(data);
     }
 }
-function dataRec(data){
+function dataRec(data) {
     console.log(data)
-    var main=data.main
-    var district=data.name
-    var weather=data.weather[0]
-    var icon=weather.icon
+    var main = data.main
+    var district = data.name
+    var weather = data.weather[0]
+    var icon = weather.icon
+    var coord = data.coord
+    var lat = coord.lat
+    var lon = coord.lon
+    var coordUrl = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
-    var iconUrl=`http://openweathermap.org/img/wn/${icon}@2x.png`
-    console.log(icon)
-    console.log(weather)
-    var temparature=main.temp
-    var cel=temparature-273.15
-   var celsius=Math.round(cel)
-   console.log(celsius)
+    fetch(coordUrl)
+        .then(res => res.json())
+        .then(data => pollutionCheck(data))
+    console.log(lat, lon)
 
-   result.innerHTML=`<h1>${celsius}° Celcius</h1> <p>${district} </p> <br> <img class="imGsize" src='${iconUrl}'>`
-   
+
+
+    var iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`
+    // console.log(icon)
+    // console.log(weather)
+    var temparature = main.temp
+    var cel = temparature - 273.15
+    var celsius = Math.round(cel)
+    //    console.log(celsius)
+
+    result.innerHTML = `<h1>${celsius}° Celcius</h1> <p>${district} </p> <br> <img class="imGsize" src='${iconUrl}'>`
+
 }
+function pollutionCheck(data) {
 
+    var list = data.list[0]
+    var aqi = list.main.aqi;
+    if (aqi == 5) {
+        var newP = document.createElement('p');
+        newP.innerText = 'Very Bad';
+        newP.classList = 'air';
+        newP.style.color='red'
+        result.appendChild(newP);
+
+
+    }
+    if (aqi == 4) {
+        var newP = document.createElement('p');
+        newP.innerText = 'Poor';
+        newP.classList = 'air';
+        newP.style.color='brown'
+        result.appendChild(newP);
+    }
+    if (aqi == 3) {
+        var newP = document.createElement('p');
+        newP.innerText = 'Moderate';
+        newP.classList = 'air';
+        newP.style.color='blue'
+        result.appendChild(newP);
+    }
+    if (aqi == 2) {
+        var newP = document.createElement('p');
+        newP.innerText = 'Fair';
+        newP.classList = 'air';
+        newP.style.color='lime'
+        result.appendChild(newP);
+    }
+    if (aqi == 1) {
+        var newP = document.createElement('p');
+        newP.innerText = 'good';
+        newP.classList = 'air';
+        newP.style.color='green'
+        result.appendChild(newP);
+    }
+}
